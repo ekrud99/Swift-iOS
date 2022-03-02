0e15aa3d44ec38fa088711569ed58e1f9f972aee
ekrud99 <dagyeong33@gmail.com>
2022-03-02 21:09:27 +0900

    Initial Commit

---
+++ My Ex 02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Ex 02
+//
+//  Created by Dagyeong on 2022/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
