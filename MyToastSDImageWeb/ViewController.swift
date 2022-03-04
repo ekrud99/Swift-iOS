4c4ac2beb852de34885695c561732c56e95c927c
origin <dagyeong33@gmail.com>
2022-03-04 23:14:13 +0900

    cocoa pods, sdwebimages, toast

--- MyToastSDImageWeb/ViewController.swift
+++ MyToastSDImageWeb/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import Toast_Swift
+import SDWebImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.makeToast("토스트 창입니다.", duration: 5.0, position: .bottom)
+        let url:URL = URL(string: "https://github.com/scalessec/Toast-Swift/raw/master/toast_swift_screenshot.jpg")!
+        self.imageView.sd_setImage(with: url, completed: {
+            (image, error, cacheType, imageURL) in
+            print(imageURL!)
+        })
+        
     }
-
+    
 
 }
 
---
+++ Pods/Target Support Files/Pods-MyToastSDImageWeb/Pods-MyToastSDImageWeb-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyToastSDImageWeb : NSObject
+@end
+@implementation PodsDummy_Pods_MyToastSDImageWeb
+@end
